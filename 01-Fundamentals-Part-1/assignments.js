852c8b69861e420a66c2ca5963f1a8fd209fb396
Leandro Higor <dev04@easyjur.com>
2020-11-28 19:44:09 -0300

    Até a aula 13 mais ou menos

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,39 @@
+// =================================================================================================================== //
+//LECTURE: Values and Variables
+
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+
+// let country = "Brazil";
+// let continent = "American continent";
+// let population = "211.8 millions";
+
+// 2. Log their values to the console
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// =================================================================================================================== //
+//LECTURE: Data Types
+
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+
+// let country = "Brazil";
+// let continent = "American continent";
+// let population = "211.8 millions";
+// let isIsland = false;
+// let language = "Portuguese";
+
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(isIsland);
+// console.log(language);
+
+// =================================================================================================================== //
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,27 @@
+// =================================================================================================================== //
+//AULA 10 - VALUES AND VARIABLES
+
+// let js = "amazing";
+// (js === "amazing") ? alert("JS is FUN!") : "";
+// console.log(40 + 8 + 23 - 10);
+
+// =================================================================================================================== //
+//AULA 12 - DATA TYPES
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 2001;
+// console.log(typeof year);
+// =================================================================================================================== //
