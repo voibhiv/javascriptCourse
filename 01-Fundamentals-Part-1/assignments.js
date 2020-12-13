332d561ce608471a6a4bfa3a9bc717da0335c0f8
Leandro Higor <leo1309melo@outlook.com>
2020-12-13 19:17:41 -0300

    AULA 21 e comentando codigo dos exercicios

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -134,11 +134,11 @@
 
 // 1. Predict the result of these 5 operations without executing them:
 
-'9' - '5'; //4
-'19' - '13' + '17';//617
-'19' - '13' + 17;//23
-'123' < 57;//FALSE
-5 + 6 + '4' + 9 - 4 - 2;//1143
+// '9' - '5'; //4
+// '19' - '13' + '17';//617
+// '19' - '13' + 17;//23
+// '123' < 57;//FALSE
+// 5 + 6 + '4' + 9 - 4 - 2;//1143
 
 // 2. Execute the operations to check if you were right
 
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -139,3 +139,29 @@
 // console.log('23' > '18');
 
 // =================================================================================================================== //
+
+// AULA 21 TRUTHY AND FALSY VALUES
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log("Height is UNDEFINED");
+}
+
