91af49b8ef0bc66e2b3e289dcc0c1e1a8f993373
Leandro Higor <leo1309melo@outlook.com>
2021-01-03 04:41:07 -0300

    aula funcoes

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -13,3 +13,26 @@
 // // const interface = 'Audio';
 // // const private = 534;
 // ===================================================================================================================
+
+// AULA 33. Functions
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
