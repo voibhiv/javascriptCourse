23e24fed71a9c11e1abb8be0ceb81383003c295e
Leandro Higor <leo1309melo@outlook.com>
2021-01-03 23:58:42 -0300

    more about function

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -16,23 +16,45 @@
 
 // AULA 33. Functions
 
-function logger() {
-    console.log('My name is Jonas');
-}
+// function logger() {
+//     console.log('My name is Jonas');
+// }
 
-//calling / running / invoking function
-logger();
-logger();
-logger();
+// //calling / running / invoking function
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// ===================================================================================================================
+
+// AULA 34. Function Declarations vs. Expressions
+
+// // function declaration
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear; 
+// }
+
+// const age1 = calcAge1(1991);
+
+// // function expression
+// const calcAge2 = function(birthYear) {
+//     return 2037 - birthYear;  
+// }
+
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+// ===================================================================================================================
