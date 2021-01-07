b8e60311927adcc0bbc380affa7720281b11ca81
Leandro Higor <leo1309melo@outlook.com>
2021-01-07 00:01:44 -0300

    aula 37 - revisando funcoes e um exercicio

---
+++ 02-Fundamentals-Part-2/assignments.js
@@ -0,0 +1,26 @@
+// ===================================================================================================================
+
+// LECTURE: Functions
+
+// // 1. Write a function called 'describeCountry' which takes three parameters:
+// // 'country', 'population' and 'capitalCity'. Based on this input, the
+// // function returns a string with this format: 'Finland has 6 million people and its
+// // capital city is Helsinki'
+
+// function describeCountry (country, population, capitalCity) {
+//     const textContent = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+//     return textContent;
+// }
+
+// // 2. Call this function 3 times, with input data for 3 different countries. Store the
+// // returned values in 3 different variables, and log them to the console
+
+// const country1 = describeCountry('Brazil', 209.5, 'Brasilia');
+// const country2 = describeCountry('Canada', 37.59, 'Ottawa');
+// const country3 = describeCountry('Argentina', 44.49, 'Buenos Aires');
+
+// console.log(country1);
+// console.log(country2);
+// console.log(country3);
+
+// ===================================================================================================================
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -93,3 +93,27 @@
 // console.log(fruitProcessor(2, 3));
 
 // ===================================================================================================================
+
+// AULA 37. Reviewing Functions
+
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         console.log(`${firstName} retires in ${retirement} year`);
+//         return retirement;
+//     } else {
+//         console.log(`${firstName} has already retired!`)
+//         return -1;
+//     }
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
+// ===================================================================================================================
