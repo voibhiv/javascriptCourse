aca6811876ac85ff727a5932f6638de36dbbe388
Leandro Higor <leo1309melo@outlook.com>
2021-01-12 22:50:46 -0300

    Um pouco sobre objetos e exercicios do proprio

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -184,3 +184,45 @@
 // console.log(neighbours);
 
 // ===================================================================================================================
+
+// LECTURE: Introduction to Objects
+
+// // 1. Create an object called 'myCountry' for a country of your choice, containing
+// // properties 'country', 'capital', 'language', 'population' and
+// // 'neighbours' (an array like we used in previous assignments)
+
+// const myCountry = {
+//     country: 'Brazil',
+//     capital: 'Brasília',
+//     language: 'Português',
+//     population: 209.5,
+//     neighbours: ['Argentina', 'Uruguai', 'Colômbia']
+// }
+
+// ===================================================================================================================
+
+// LECTURE: Dot vs. Bracket Notation
+
+// // 1. Using the object from the previous assignment, log a string like this to the
+// // console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+// // and a capital called Helsinki.'
+
+// const myCountry = {
+//     country: 'Brazil',
+//     capital: 'Brasília',
+//     language: 'Português',
+//     population: 209.5,
+//     neighbours: ['Argentina', 'Uruguai', 'Colômbia']
+// }
+
+// console.log(`${myCountry['country']} has ${myCountry['population']} finnish-speaking people, ${myCountry['neighbours'].length} neighbouring countries and a capital called ${myCountry['capital']}`);
+
+// // 2. Increase the country's population by two million using dot notation, and then
+// // decrease it by two million using brackets notation.
+
+// myCountry.population = myCountry.population + 2;
+// console.log(myCountry);
+// myCountry['population'] = myCountry['population'] - 2;
+// console.log(myCountry);
+
+// ===================================================================================================================
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -192,3 +192,62 @@
 // console.log(friends.includes('Bob'));
 
 // ===================================================================================================================
+
+// AULA 42. Introduction to Objects
+
+// const jonasArray = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991, 
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven']
+// ]
+
+// // Sintaxe Object
+// const jonas = {
+//     firstName : 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// }
+
+// ===================================================================================================================
+
+// AULA 43. Dot vs. Bracket Notation
+
+// const jonas = {
+//     firstName : 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// }
+
+// console.log(jonas);
+
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
+
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
+
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+
+// if (jonas[interestedIn]) {
+//     console.log(jonas[interestedIn]);
+// } else {
+//     console.log("Wrong request ! Choose between firstName, lastName, age, job and friends");
+// }
+
+// // Adicionando valores
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschmedtman';
+// console.log(jonas);
+
+// // Challenge
+// // Jonas has 3 friends, and his best friend is called Michael
+// console.log(`${jonas['firstName']} has ${jonas['friends'].length} friends, and his best friend is ${jonas['friends'][0]}`);
+
+// ===================================================================================================================
