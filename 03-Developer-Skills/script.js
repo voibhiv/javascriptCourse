085c90972755d439c2d237b75cf2e660d5bdc7d0
Leandro Higor <leo1309melo@outlook.com>
2021-01-25 22:22:12 -0300

    uso de snippets

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,2 +1,16 @@
+// @ts-nocheck
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+'use strict'; 
+// ====================================================================================================================
+
+// AULA 55. Setting up Prettier and VS Code
+
+// const  x = 23;
+// if (x === 3) console.log(23);
+
+// const calcAge = birthYear => 2037 - birthYear;
+
+// console.log();
+
+// ===================================================================================================================
+
