a4c0420f40ddc5de5bce15fe6799667e24a65f8c
Leandro Higor <leo1309melo@outlook.com>
2021-02-08 23:50:25 -0300

    inicio projeto 1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,10 +1,19 @@
 // @ts-nocheck
 'use script';
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 13;
+// document.querySelector('.number').textContent = 13;
 
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 15;
+// document.querySelector('.guess').value = 15;
+
+document.querySelector('.check').addEventListener('click' , function() {
+    const guess = Number(document.querySelector('.guess').value);
+    
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!';
+    }
+
+});
