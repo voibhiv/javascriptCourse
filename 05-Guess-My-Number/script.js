b2c7ac34344af534f56cae780c3663006417b328
Leandro Higor <leo1309melo@outlook.com>
2021-02-09 23:54:06 -0300

    primeiros passos logica game

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -9,11 +9,37 @@
 
 // document.querySelector('.guess').value = 15;
 
-document.querySelector('.check').addEventListener('click' , function() {
-    const guess = Number(document.querySelector('.guess').value);
-    
-    if (!guess) {
-        document.querySelector('.message').textContent = '⛔ No number!';
-    }
+const secretNumber = Math.trunc(Math.random() * 20) + 1; 
+let score = 20;
 
+console.log(secretNumber);
+document.querySelector('.check').addEventListener('click' , function() {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = number;
+    document.querySelector('.highscore').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  
 });
