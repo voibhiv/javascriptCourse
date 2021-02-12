e174559fa3f84e6876f657bb6c54b0f5d87b4ee4
Leandro Higor <leo1309melo@outlook.com>
2021-02-11 23:59:02 -0300

    aplicando efeitos quando certo

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -20,8 +20,11 @@ document.querySelector('.check').addEventListener('click' , function() {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-    document.querySelector('.number').textContent = number;
-    document.querySelector('.highscore').textContent = score;
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 too high!';
