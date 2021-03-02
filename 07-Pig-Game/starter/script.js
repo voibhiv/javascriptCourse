e90487b31ef7b1a071cef8d9fda8f665f45e5b27
Leandro Higor <leo1309melo@outlook.com>
2021-03-01 23:32:09 -0300

    começo do ultimo projeto secao 7

---
+++ 07-Pig-Game/starter/script.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+'use strict';
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEL   = document.querySelector('.dice');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEL.classList.add('hidden'); 
