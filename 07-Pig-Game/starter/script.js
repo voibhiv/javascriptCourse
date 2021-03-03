888c3970a0032c78ffdd2589d40947310f726d3a
Leandro Higor <leo1309melo@outlook.com>
2021-03-02 23:58:52 -0300

    continuacao game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,11 +2,42 @@
 'use strict';
 
 // Selecting Elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
-const diceEL   = document.querySelector('.dice');
+const score0El   = document.querySelector('#score--0');
+const score1El   = document.querySelector('#score--1');
+const currentEl0 = document.querySelector('#current--0');
+const currentEl1 = document.querySelector('#current--1');
+const diceEL     = document.querySelector('.dice');
+const btnNew     = document.querySelector('.btn--new');
+const btnRoll    = document.querySelector('.btn--roll');
+const btnHold    = document.querySelector('.btn--hold');
 
 // Starting Conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEL.classList.add('hidden'); 
+
+let currentScore = 0;
+
+// Rolling dice funcionality
+btnRoll.addEventListener('click', function() {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc((Math.random() * 6)) + 1;
+  console.log(dice);
+
+
+  // 2. Display dice
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1)  {
+    // Add dice to current score
+    currentScore += dice;
+    currentEl0.textContent = currentScore;
+  } else {
+    // Switch to next player
+
+
+  }
+
+});
