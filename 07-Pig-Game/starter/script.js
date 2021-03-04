16f1c123e00f31a25ffd56f49d4840ff2a5fb5d1
Leandro Higor <leo1309melo@outlook.com>
2021-03-03 23:56:24 -0300

    continuacao pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,9 +2,11 @@
 'use strict';
 
 // Selecting Elements
+const player0El  = document.querySelector('.player--0');
+const player1El  = document.querySelector('.player--1');
 const score0El   = document.querySelector('#score--0');
 const score1El   = document.querySelector('#score--1');
-const current0El = document.querySeElctor('#current--0');
+const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
 const diceEL     = document.querySelector('.dice');
 const btnNew     = document.querySelector('.btn--new');
@@ -16,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEL.classList.add('hidden'); 
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice funcionality
 btnRoll.addEventListener('click', function() {
@@ -33,11 +37,14 @@ btnRoll.addEventListener('click', function() {
   if (dice !== 1)  {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // Switch to next player
-
-
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 
 });
