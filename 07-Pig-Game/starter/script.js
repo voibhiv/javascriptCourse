cbb46461907c90143822dfa926395d9d2a35855e
Leandro Higor <leo1309melo@outlook.com>
2021-03-05 21:52:39 -0300

    fim do game pig

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,15 +13,24 @@ const btnNew     = document.querySelector('.btn--new');
 const btnRoll    = document.querySelector('.btn--roll');
 const btnHold    = document.querySelector('.btn--hold');
 
-// Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEL.classList.add('hidden'); 
+let currentScore, activePlayer, playing, scores
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting Conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  diceEL.classList.add('hidden'); 
+  player0El.classList.remove('player-winner');
+  player1El.classList.remove('player-winner');
+  player0El.classList.add('player-active');
+  player1El.classList.remove('player-active');
+};
+init();
 
 const switchPlayer = function() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -77,3 +86,8 @@ btnHold.addEventListener('click', function() {
   }
 
 });
+
+btnNew.addEventListener('click', function() {
+  init();
+}); 
+
