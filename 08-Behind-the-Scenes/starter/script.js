d9c509bceedb4964d84cd84464c5a09d89bc5fad
Leandro Melo <leo1309melo@hotmail.com>
2021-03-21 20:21:19 -0300

    aula 96 - about Hoisting

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -35,3 +35,36 @@ function calcAge(birthYear) {
 const firstName = 'Jonas';
 calcAge(1991);
 
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+
+console.log(addDecl(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+function addDecl (a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+
+// Example 
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart(); 
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
