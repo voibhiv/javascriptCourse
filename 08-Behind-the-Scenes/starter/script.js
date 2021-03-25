d823aec58b5590b3e2fe2cc5425313efe9f7f421
Leandro Melo <leo1309melo@hotmail.com>
2021-03-24 23:58:18 -0300

    mais sobre this

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -53,9 +53,7 @@
 //   return a + b;
 // }
 
-// const addExpr = function (a, b) {
-//   return a + b;
-// }
+
 
 // const addArrow = (a, b) => a + b;
 
@@ -69,34 +67,70 @@
 //   console.log('All products deleted!');
 // }
 
-const calcAge = function () {
-  console.log(2037 - birthYear);
-  // console.log(this);
-}
+// const calcAge = function () {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// }
 
-calcAge(1991);
+// calcAge(1991);
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  // console.log(this);
-}
-calcAgeArrow(1980);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// }
+// calcAgeArrow(1980);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   }
+// }
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// }
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
 
 const jonas = {
+  firstName: 'Jonas',
   year: 1991,
   calcAge: function () {
-    console.log(this);
+    // console.log(this);
     console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenal = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996)
+    // };
+
+    const isMillenal =  () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996)
+    };
+    isMillenal();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
   }
-}
+
+};
+
 jonas.calcAge();
+jonas.greet();
 
-const matilda = {
-  year: 2017,
+const addExpr = function (a, b) {
+  console.log(arguments); //no exist in arrow functions
+  return a + b;
 }
-
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f();
+addExpr(2,5,9,7);
