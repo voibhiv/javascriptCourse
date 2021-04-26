3794617b612e0fe4bbe875c8733d2879582fc49e
Leandro Melo <leo1309melo@hotmail.com>
2021-04-25 23:45:45 -0300

    spread objects and in functions

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -40,6 +40,8 @@ const restaurant = {
   },
 };
 
+// 1) Destructuring
+
 // SPREAD, becaus on RIGHT side of =
 const arr = [1, 2, ...[3, 4]]
 
@@ -53,7 +55,24 @@ const [pizza, , risotto, ...otherFood] = [
 ];
 console.log(pizza, risotto, otherFood);
 
+// Objects
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays);
 
+// 2) Functions
+const add = function(...numbers) {
+  let sum = 0;
+
+  for (let i = 0; i <= numbers.length - 1; i++) {
+    sum += numbers[i];
+  }
+
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
 
 /////////////////////////////
 // THE SPREAD OPERATOR
