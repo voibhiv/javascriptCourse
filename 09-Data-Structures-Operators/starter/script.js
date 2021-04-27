0afac9aaf298b981d8bf50a8f3dead940a80da9e
Leandro Melo <leo1309melo@hotmail.com>
2021-04-26 23:10:35 -0300

    last lecture about spread

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -24,6 +24,12 @@ const restaurant = {
     console.log(`Here is delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
   },
 
+  orderPizza: function(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+    
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -73,6 +79,12 @@ const add = function(...numbers) {
 add(2, 3);
 add(5, 3, 7, 2);
 add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
 
 /////////////////////////////
 // THE SPREAD OPERATOR
