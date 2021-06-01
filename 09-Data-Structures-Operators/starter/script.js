0889c16a7fc3e4dbff18ee24552c182f6af32d22
Leandro Melo <leo1309melo@hotmail.com>
2021-05-31 23:32:33 -0300

    for of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -46,6 +46,18 @@ const restaurant = {
   },
 };
 
+///////////////////////////////////////////
+// forOfLoop
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const [i, el] of menu.entries()) {
+//     console.log(`${i + 1}: ${el}`);
+// }
+
+///////////////////////////////////////////
 // Nulish 
 
 // restaurant.numGuests = 0;
