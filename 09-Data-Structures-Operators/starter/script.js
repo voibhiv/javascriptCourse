28fc40076550618aaf3c490440dae59f5978c3d3
Leandro Melo <leo1309melo@hotmail.com>
2021-06-06 19:23:02 -0300

    Optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,6 +50,29 @@ const restaurant = {
 
 };
 console.log(restaurant.openingHours);
+
+///////////////////////////////////////////
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open;
+  console.log(`On ${day} ,we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{name: 'Jonas', email: 'hello@jonas.io'}];
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name)
+else console.log('user array empty');
+
 ///////////////////////////////////////////
 // forOfLoop
 
