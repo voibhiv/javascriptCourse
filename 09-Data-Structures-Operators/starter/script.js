841f7a059f1a48e3c91037edb6be214838a924ba
Leandro Melo <leo1309melo@hotmail.com>
2021-06-13 22:47:12 -0300

    Maps Interation

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,39 +54,72 @@ const restaurant = {
 
 ///////////////////////////////////////////
 // MAPS METHOD
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
 
-rest.set('categories', ['Italian',, 'Pizzeria', 'Vegetarian', 'Organic'])
-    .set('open', 11)
-    .set('close', 23)
-    .set(true, 'We are open :D')
-    .set(false, 'We are closed :( ');
+// rest.set('categories', ['Italian',, 'Pizzeria', 'Vegetarian', 'Organic'])
+//     .set('open', 11)
+//     .set('close', 23)
+//     .set(true, 'We are open :D')
+//     .set(false, 'We are closed :( ');
 
-console.log(rest.get('name'));
-rest.set('name', rest.get('name') + ' olalala'); // Important
-console.log(rest.get('name'));
+// console.log(rest.get('name'));
+// rest.set('name', rest.get('name') + ' olalala'); // Important
+// console.log(rest.get('name'));
 
-console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-// rest.clear();
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear();
 
-const arr = [1, 2];
-rest.set(arr, 'Test');
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-console.log(rest.size);
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
 
-console.log(rest.get(arr));
+// console.log(rest.get(arr));
 
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try again!']
+
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hourMap = new Map(Object.entries(openingHours));
+
+console.log(hourMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = 3;
+// const answer = Number(prompt('Your answer'));
+console.log(question.get(question.get('correct') === answer));
+
+// COnvert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
 
 ///////////////////////////////////////////
 // SET METHOD 
